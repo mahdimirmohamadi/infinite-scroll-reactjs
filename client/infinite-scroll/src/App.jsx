cd32ad91b05715834ecdef611a5d280556d09923
Mahdi MirMohamadi <mahdimirmohamadi13@gmail.com>
2023-12-04 02:07:22 +0330

    handle fetching new photos at the end of the screen

--- client/infinite-scroll/src/App.jsx
+++ client/infinite-scroll/src/App.jsx
@@ -1,18 +1,52 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./styles.css";
+import { parseLinkHeader } from "./parseLinkHeader";
 
 export default function App() {
   const [photos, setPhotos] = useState([]);
+  const nextPhotoUrlRef = useRef();
+
+  async function fetchPhotos(url, { overwrite = false } = {}) {
+    const res = await fetch(url);
+    nextPhotoUrlRef.current = parseLinkHeader(res.headers.get("Link")).next;
+    const photos = await res.json();
+    if (overwrite) {
+      setPhotos(photos);
+    } else {
+      setPhotos((previousPhotos) => {
+        return [...previousPhotos, ...photos];
+      });
+    }
+  }
+
+  const imageRef = useCallback((image) => {
+    if (image == null) return;
+
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        console.log("last is shown");
+        fetchPhotos(nextPhotoUrlRef.current);
+        observer.unobserve(image);
+      }
+    });
+
+    observer.observe(image);
+  }, []);
+
   useEffect(() => {
-    fetch("http://localhost:3000/photos?_page=1&_limit=10")
-      .then((res) => res.json())
-      .then(setPhotos);
+    fetchPhotos("http://localhost:3000/photos?_page=1&_limit=10", {
+      overwrite: true,
+    });
   }, []);
 
   return (
     <div className="grid">
-      {photos?.map((photo) => (
-        <img src={photo.url} key={photo.id} />
+      {photos?.map((photo, index) => (
+        <img
+          src={photo.url}
+          key={photo.id}
+          ref={index === photos.length - 1 ? imageRef : undefined}
+        />
       ))}
     </div>
   );
